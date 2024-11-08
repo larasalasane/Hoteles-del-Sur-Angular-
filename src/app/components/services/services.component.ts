a617fe00cbaafdcdfa50e6715a9cfec89dd8b2b7
Cristian Moraleda <cristian.moraleda.-nd@disney.com>
2024-11-08 14:51:37 -0300

    Style changes

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ServicesComponent {
   services = [
-    { id: 'rnfyo3k', title: 'Habitaciones confortables', description: 'Disfruta de nuestras habitaciones con todas las comodidades.' }
+    { id: 'rnfyo3k', title: 'Ballenas confortables', description: 'Disfruta de nuestras ballenas con todas las comodidades.' },
+    { id: 'z1jrdyh', title: 'Spa confortables', description: 'Disfruta de nuestras habitaciones con todas las comodidades.' },
+    { id: 'ck3kcfd', title: 'Pinguinos confortables', description: 'Disfruta de nuestros pinguinos con todas las comodidades.' }
   ];
   //esto tendria que venir de la base de datos
   // por cada servicio va a crear un componente con toda la informacion
