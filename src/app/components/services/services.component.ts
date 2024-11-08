8f167daeec4f22810e08db66e7c7d20bc5a0b59a
whtshdowmdq <facucuadradomdq@gmail.com>
2024-11-08 16:15:54 -0300

    Cambios para uso de Servicios

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicesService } from '../../services/services.service';
+import { Service } from '../../models/service.model';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent {
-  services = [
-    { id: 'rnfyo3k', title: 'Ballenas confortables', description: 'Disfruta de nuestras ballenas con todas las comodidades.' },
-    { id: 'z1jrdyh', title: 'Spa confortables', description: 'Disfruta de nuestras habitaciones con todas las comodidades.' },
-    { id: 'ck3kcfd', title: 'Pinguinos confortables', description: 'Disfruta de nuestros pinguinos con todas las comodidades.' }
-  ];
-  //esto tendria que venir de la base de datos
-  // por cada servicio va a crear un componente con toda la informacion
+export class ServicesComponent implements OnInit{
+  services : Service[] | undefined ;
 
-  constructor() {}
+  constructor(private servicesService : ServicesService) {}
+
+  ngOnInit(): void {
+      this.servicesService.getAllServices().then(
+        foundServices => this.services = foundServices
+      ).catch(
+        
+      )
+      
+  }
 }
---
+++ src/app/models/service.model.ts
@@ -0,0 +1,6 @@
+export class Service{
+    id: string | undefined;
+    title: string | undefined;
+    description: string | undefined;
+}
+
