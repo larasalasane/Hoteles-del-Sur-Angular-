ef795ea70f8715c3beb871f370756df92caabab6
Cristian David Moraleda <73443385+Sieglind@users.noreply.github.com>
2024-11-15 18:37:50 -0300

    Creando Pagina Principal (#22)
    * primeros-cambios
    
    * segundo cambio
    
    ---------
    
    Co-authored-by: larasalasane <larasalasane@gmail.com>

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {EventBusService} from '../../services/event-bus.service';
+import { Component, OnInit } from '@angular/core';
+import { EventBusService } from '../../services/event-bus.service';
+import { PexelService } from '../../services/pexel.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,14 +9,29 @@ import {EventBusService} from '../../services/event-bus.service';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-  constructor(private eventBus: EventBusService) {
+  photoMedia: any[] = [];
+  photoCount: number = 0;
+
+  constructor(private pexelService: PexelService, private eventBus: EventBusService, private sanitizer: DomSanitizer) {
   }
 
   showReservationForm: boolean = false;
 
   ngOnInit() {
+    this.getServiceMedia();
     this.eventBus.toggleForm$.subscribe(() => {
       this.showReservationForm = !this.showReservationForm;
     });
+  }
+
+
+  async getServiceMedia(): Promise<void> {
+    try {
+      const collection = await this.pexelService.getCollectionMedia('imbkzhu');
+      if (collection) {
+        this.photoMedia = collection.media.filter(media => media.type === 'Photo');
+        this.photoCount = this.photoMedia.length;
+      }
+    } catch (error) { console.error('Error fetching media:', error); }
   }
 }
