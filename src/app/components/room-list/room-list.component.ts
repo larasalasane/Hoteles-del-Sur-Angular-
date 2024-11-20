0688fd994b3f5ba30b8925b982919679f53233a1
joaquina <joaquinamariatobler@gmail.com>
2024-11-19 22:40:16 -0300

    Filtrado para lista habitaciones

--- src/app/components/room-list/room-list.component.ts
+++ src/app/components/room-list/room-list.component.ts
@@ -13,6 +13,10 @@ export class RoomListComponent implements OnInit {
 
   rooms: any[] = [];
 
+  filterText: string = '';
+
+  filteredRooms: any[] = [];
+
   constructor(
     private roomService: RoomDataService,
     public dialog: MatDialog,
@@ -21,6 +25,7 @@ export class RoomListComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     await this.getRooms();
+    this.filteredRooms = this.rooms;
   }
 
   async openConfirmDialog(roomId: string): Promise<void> {
@@ -38,13 +43,51 @@ export class RoomListComponent implements OnInit {
 
   }
 
+  applyPriceFilter(filter: string): void {
+    this.filterText = filter;
+
+    switch (filter) {
+      case 'below10k':
+        this.filteredRooms = this.rooms.filter(room => room.details.pricePerNight <= 10);
+        break;
+      case '10kto80k':
+        this.filteredRooms = this.rooms.filter(room => room.details.pricePerNight > 10 && room.details.pricePerNight <= 80);
+        break;
+      case 'above80k':
+        this.filteredRooms = this.rooms.filter(room => room.details.pricePerNight > 80);
+        break;
+      default:
+        this.filteredRooms = [...this.rooms]; // Si no hay filtro, muestra todas
+    }
+  }
+
+  /*
+
+  applyFilter(): void {
+    console.log('Texto de filtro:', this.filterText);
+    console.log('Habitaciones filtradas:', this.filteredRooms); 
+  
+    const lowerCaseFilter = this.filterText.trim().toLowerCase();
+    this.filteredRooms = this.rooms.filter(room =>
+      room.details.pricePerNight.toString().includes(lowerCaseFilter)
+    );
+  }
+    */
+  
   async cancelRoom(roomId: string): Promise<void> {
     await this.roomService.deleteRoom(roomId);
     await this.getRooms()
   }
 
+  // async getRooms(): Promise<void> {
+  //   const rooms = this.roomService.getRooms().subscribe(rooms => rooms ? this.rooms = rooms : []);
+  // }
+
   async getRooms(): Promise<void> {
-    const rooms = this.roomService.getRooms().subscribe(rooms => rooms ? this.rooms = rooms : []);
+    this.roomService.getRooms().subscribe(rooms => {
+      this.rooms = rooms ? rooms : [];
+      this.filteredRooms = [...this.rooms]; // Sincroniza filteredRooms
+    });
   }
 
   goToEditRooms(roomId: string): void {
