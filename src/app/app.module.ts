323f16c4dd9991ccf5ac8b0cb7337b42753137f5
Sieglind <moraledacd@gmail.com>
2024-11-24 19:12:38 -0300

    Final Step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {provideHttpClient} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
 
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDialogModule } from '@angular/material/dialog';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDialogModule} from '@angular/material/dialog';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
-import { ReservationService } from './reservation/reservation.service';
+import {ReservationService} from './reservation/reservation.service';
 
-import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { ServiceModule } from './service/service.module';
-import { RoomModule } from './room/room.module';
-import { ReservationModule } from './reservation/reservation.module';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { AuthModule } from './auth/auth.module';
-import { CoreModule } from './core/core.module';
-import { ContactModule } from './contact/contact.module';
-import { ProfileModule } from './profile/profile.module';
-import { LandingPageModule } from './landing-page/landing-page.module';
-import { SharedModule } from './shared/shared.module';
+import {CommonModule} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {ServiceModule} from './service/service.module';
+import {RoomModule} from './room/room.module';
+import {ReservationModule} from './reservation/reservation.module';
+import {DashboardModule} from './dashboard/dashboard.module';
+import {AuthModule} from './auth/auth.module';
+import {CoreModule} from './core/core.module';
+import {ContactModule} from './contact/contact.module';
+import {ProfileModule} from './profile/profile.module';
+import {LandingPageModule} from './landing-page/landing-page.module';
+import {SharedModule} from './shared/shared.module';
 
 
 @NgModule({
@@ -66,4 +66,5 @@ import { SharedModule } from './shared/shared.module';
   providers: [ReservationService, provideHttpClient()],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/room/add-rooms/add-rooms.component.ts
+++ src/app/room/add-rooms/add-rooms.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {RoomDataService} from '../room-data.service';
 import {CustomValidators} from '../../core/validators/custom-validators';
 import {Router} from '@angular/router';
+import {RoomService} from '../room.service';
 
 
 @Component({
@@ -17,11 +17,11 @@ export class AddRoomsComponent {
 
   constructor(
     private fb: FormBuilder,
-    private roomDataService: RoomDataService,
-    private router: Router
+    private router: Router,
+    private roomService: RoomService,
   ) {
     this.roomForm = this.fb.group({
-      id: ['', Validators.required],
+      id: ['', Validators.required, CustomValidators.roomExists(this.roomService)],
       type: ['', Validators.required],
       details: this.fb.group({
         capacity: ['', [Validators.required, Validators.min(1), Validators.max(4)]],
@@ -34,11 +34,15 @@ export class AddRoomsComponent {
 
   async onSubmit(): Promise<void> {
     if (this.roomForm.valid) {
-      this.roomDataService.createRoom(this.roomForm.value).subscribe(
+      this.roomService.createRoom(this.roomForm.value).subscribe(
         (createdRoom) => {
-          this.successMessage = true;
-          setTimeout(() => this.successMessage = false, 3000);
-          this.router.navigateByUrl(`/rooms/${createdRoom.id}`);
+          if (createdRoom) {
+            this.successMessage = true;
+            setTimeout(() => this.successMessage = false, 3000);
+            this.router.navigateByUrl(`/rooms/${createdRoom.id}`);
+          } else {
+            throw new Error("Error desconocido al crear habitacion")
+          }
         }, (error) => console.error('Error al añadir la habitación:', error));
     } else {
       console.log('El formulario es inválido');
